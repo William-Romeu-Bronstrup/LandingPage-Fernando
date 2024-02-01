c317a86b9f72bfe6676f6d5edcd9dfac59d63394
William Romeu <williambronstrup13@gmail.com>
2024-02-01 15:42:21 -0300

    add web analytics by vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 })
